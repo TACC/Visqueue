3f23763b0d2d9681891beb01b379cc61502c2330
ajs2987 <asolis@tacc.utexas.edu>
2019-08-07 16:43:58 -0500

    render multiple graphs on the same page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 import { SCWAT } from './scwat.js';
 
-let scwat = new SCWAT( 'data.json', 'stampede2sunburst', 'div', 450, 450 );
+let stampede = new SCWAT( 'stampede2.json', 'stampedeSunburst', 'div', 450, 450 );
+let maverick = new SCWAT( 'maverick2.json', 'maverickSunburst', 'div', 450, 450 );
+let lonestar = new SCWAT( 'lonestar5.json', 'lonestarSunburst', 'div', 450, 450 );
 
-scwat.render();
+stampede.render();
+maverick.render();
+lonestar.render();

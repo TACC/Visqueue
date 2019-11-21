76aafd9424c49b265a840933a22a7346a6f23631
Andrew Solis <asolis@tacc.utexas.edu>
2019-11-21 12:58:49 -0600

    add two new graphs that are placeholders for now until database is complete

--- src/app/detail/info/info.component.ts
+++ src/app/detail/info/info.component.ts
@@ -16,7 +16,7 @@ export class InfoComponent implements OnInit
         title : 
         { 
             display : true,
-            text    : 'Top 10 Field of Sciences Organized by # of Projects'
+            text    : 'Top 10 Field of Sciences by # of Projects'
         },
         responsive : true,
         // We use these empty structures as placeholders for dynamic theming.
@@ -43,7 +43,7 @@ export class InfoComponent implements OnInit
         title : 
         { 
             display : true,
-            text    : 'Top 10 Field of Sciences Organized by # of Jobs'
+            text    : 'Top 10 Field of Sciences by # of Jobs'
         },
         responsive : true,
         // We use these empty structures as placeholders for dynamic theming.
@@ -64,6 +64,58 @@ export class InfoComponent implements OnInit
         }
     };
 
+    public fosByNodesBarChartOptions : ChartOptions = 
+    {
+        title : 
+        { 
+            display : true,
+            text    : 'Top 10 Field of Sciences by # of Nodes'
+        },
+        responsive : true,
+        // We use these empty structures as placeholders for dynamic theming.
+        scales: {
+            xAxes: [ { } ],
+            yAxes: 
+            [ 
+                { ticks : { beginAtZero : true }, scaleLabel : { display : true, labelString : '# Nodes' } }
+            ]
+        },
+        plugins:
+        {
+            datalabels:
+            {
+                anchor: 'end',
+                align: 'end',
+            }
+        }
+    };
+
+    public fosByDurationBarChartOptions : ChartOptions = 
+    {
+        title : 
+        { 
+            display : true,
+            text    : 'Top 10 Field of Sciences by Duration'
+        },
+        responsive : true,
+        // We use these empty structures as placeholders for dynamic theming.
+        scales: {
+            xAxes: [ { } ],
+            yAxes: 
+            [ 
+                { ticks : { beginAtZero : true }, scaleLabel : { display : true, labelString : 'Duration' } }
+            ]
+        },
+        plugins:
+        {
+            datalabels:
+            {
+                anchor: 'end',
+                align: 'end',
+            }
+        }
+    };
+
     public fosByProjBarChartLabels: Label[] = [];
     public fosByProjBarChartType: ChartType = 'bar';
     public fosByProjBarChartLegend = false;
@@ -71,6 +123,14 @@ export class InfoComponent implements OnInit
     public fosByJobBarChartLabels: Label[] = [];
     public fosByJobBarChartType: ChartType = 'bar';
     public fosByJobBarChartLegend = false;
+
+    public fosByNodesBarChartLabels: Label[] = [];
+    public fosByNodesBarChartType: ChartType = 'bar';
+    public fosByNodesBarChartLegend = false;
+
+    public fosByDurationBarChartLabels: Label[] = [];
+    public fosByDurationBarChartType: ChartType = 'bar';
+    public fosByDurationBarChartLegend = false;
 
     public fosByProjBarChartData: ChartDataSets[] = [
         {
@@ -112,6 +172,46 @@ export class InfoComponent implements OnInit
         }
     ];
 
+    public fosByNodesBarChartData: ChartDataSets[] = [
+        {
+            label : '# Nodes',
+            data : [],
+            backgroundColor :
+            [
+                'rgba(77,121,168, 0.8)',
+                'rgba(242, 142, 48, 0.8)',
+                'rgba(225,87,88, 0.8)',
+                'rgba(118,183,178, 0.8)',
+                'rgba(89,161,78, 0.8)',
+                'rgba(237,201,72, 0.8)',
+                'rgba(175,122,161, 0.8)',
+                'rgba(242,156,166, 0.8)',
+                'rgba(156,117,95, 0.8)',
+                'rgba(186,176,171, 0.8)'
+            ]
+        }
+    ];
+
+    public fosByDurationBarChartData: ChartDataSets[] = [
+        {
+            label : 'Duration',
+            data : [],
+            backgroundColor :
+            [
+                'rgba(77,121,168, 0.8)',
+                'rgba(242, 142, 48, 0.8)',
+                'rgba(225,87,88, 0.8)',
+                'rgba(118,183,178, 0.8)',
+                'rgba(89,161,78, 0.8)',
+                'rgba(237,201,72, 0.8)',
+                'rgba(175,122,161, 0.8)',
+                'rgba(242,156,166, 0.8)',
+                'rgba(156,117,95, 0.8)',
+                'rgba(186,176,171, 0.8)'
+            ]
+        }
+    ];
+
     fosTotal         : number;
     projectsTotal    : number;
     jobsTotal        : number;
@@ -133,7 +233,6 @@ export class InfoComponent implements OnInit
 
                 for ( let index = 0; index < 10; index++ )
                 {
-                    console.log( data.fosByProj[ index ] );
 
                     this.fosByProjBarChartLabels.push( data.fosByProj[ index ].name );
                     this.fosByProjBarChartData[0].data.push( data.fosByProj[ index ].value );

6c224c40cbeefc57bfda089591e73856c97fdbb8
Andrew Solis <asolis@tacc.utexas.edu>
2019-11-21 16:21:43 -0600

    add sorting to table

--- src/app/detail/info/info-table/info-table.component.ts
+++ src/app/detail/info/info-table/info-table.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
-export interface TableInfoElement
-{
-  fos      : string;
-  projects : number;
-  jobs     : number;
-  nodes    : number;
-  duration : number;
+export interface TableInfoElement {
+  fos: string;
+  projects: number;
+  jobs: number;
+  nodes: number;
+  duration: number;
 }
 
 const ELEMENT_DATA: TableInfoElement[] = [
-  { fos: 'Materials Research (DMR)', projects: 100,  jobs: 10000, nodes: 10000,  duration : 1000000 },
-  { fos: 'BioPhysics',               projects: 200,  jobs: 9000,  nodes: 20000,  duration : 900000 },
-  { fos: 'Fluid Dynamics',           projects: 300,  jobs: 8000,  nodes: 30000,  duration : 800000 },
-  { fos: 'Astronomical',             projects: 400,  jobs: 7000,  nodes: 40000,  duration : 700000 },
-  { fos: 'Physics',                  projects: 500,  jobs: 6000,  nodes: 50000,  duration : 600000 },
-  { fos: 'Engineering',              projects: 600,  jobs: 5000,  nodes: 60000,  duration : 500000 },
-  { fos: 'Training',                 projects: 700,  jobs: 4000,  nodes: 70000,  duration : 400000 },
-  { fos: 'Chemistry',                projects: 800,  jobs: 3000,  nodes: 80000,  duration : 300000 },
-  { fos: 'ASC',                      projects: 900,  jobs: 2000,  nodes: 90000,  duration : 200000 },
-  { fos: 'Visualization',            projects: 1000, jobs: 1000,  nodes: 100000, duration : 100000 }
+  { fos: 'Materials Research (DMR)', projects: 100, jobs: 10000, nodes: 10000, duration: 1000000 },
+  { fos: 'BioPhysics', projects: 200, jobs: 9000, nodes: 20000, duration: 900000 },
+  { fos: 'Fluid Dynamics', projects: 300, jobs: 8000, nodes: 30000, duration: 800000 },
+  { fos: 'Astronomical', projects: 400, jobs: 7000, nodes: 40000, duration: 700000 },
+  { fos: 'Physics', projects: 500, jobs: 6000, nodes: 50000, duration: 600000 },
+  { fos: 'Engineering', projects: 600, jobs: 5000, nodes: 60000, duration: 500000 },
+  { fos: 'Training', projects: 700, jobs: 4000, nodes: 70000, duration: 400000 },
+  { fos: 'Chemistry', projects: 800, jobs: 3000, nodes: 80000, duration: 300000 },
+  { fos: 'ASC', projects: 900, jobs: 2000, nodes: 90000, duration: 200000 },
+  { fos: 'Visualization', projects: 1000, jobs: 1000, nodes: 100000, duration: 100000 }
 ];
 
 @Component({
@@ -29,12 +30,15 @@ const ELEMENT_DATA: TableInfoElement[] = [
 })
 export class InfoTableComponent implements OnInit {
 
-  displayedColumns: string[] = ['fos', 'projects', 'jobs', 'nodes', 'duration' ];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['fos', 'projects', 'jobs', 'nodes', 'duration'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
   }
 
 }
